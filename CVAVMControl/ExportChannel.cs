3314b54334810763d91f195e0bf77e55320c50f2
21158 <21158@GU>
2025-12-12 17:48:40 +0800

    VAM更新

---
+++ CVAVMControl/ExportChannel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVAVMControl
+{
+    /// <summary>
+    /// 导出通道枚举
+    /// </summary>
+    public enum ExportChannel
+    {
+        /// <summary>
+        /// X通道
+        /// </summary>
+        X,
+
+        /// <summary>
+        /// Y通道
+        /// </summary>
+        Y,
+
+        /// <summary>
+        /// Z通道
+        /// </summary>
+        Z
+    }
+}
--- VAMdemo/obj/Debug/net8.0-windows/ConoscopeDemo.AssemblyInfo.cs
+++ VAMdemo/obj/Debug/net8.0-windows/ConoscopeDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0109ad658911ef752ae8b15b4bb1ee16eee13cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e335846a9c5355ecba7faf5a3c17ec07a00ba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
