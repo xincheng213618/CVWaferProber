13e3206a7c3c07aa7a124237b97235f6ef237082
21158 <21158@GU>
2025-12-17 11:47:02 +0800

    自动导出CSV

--- VAMdemo/obj/Debug/net8.0-windows/ConoscopeDemo.AssemblyInfo.cs
+++ VAMdemo/obj/Debug/net8.0-windows/ConoscopeDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f977efc74919b98014d793ce108c367a7a1ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fb0daa1b87e8c39084daa899672f74fe814979")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
