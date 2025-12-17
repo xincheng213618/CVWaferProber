3a072a61b92cf824fe3a757f507beedfa58b9fcc
chenhong <chenhong@color-vision.com.cn>
2025-12-17 15:18:50 +0800

    up

--- VAMdemo/obj/Debug/net8.0-windows/App.g.i.cs
+++ VAMdemo/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ConoscopeDemo {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ConoscopeDemo {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public static void Main() {
             ConoscopeDemo.App app = new ConoscopeDemo.App();
             app.InitializeComponent();
--- VAMdemo/obj/Debug/net8.0-windows/ConoscopeDemo.AssemblyInfo.cs
+++ VAMdemo/obj/Debug/net8.0-windows/ConoscopeDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fb0daa1b87e8c39084daa899672f74fe814979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03175fd9042becda2ec57d3ef210cc17369e3c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConoscopeDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
