be337d3b11199414433c1ee11169ffdfd561ef95
chenhong <chenhong@color-vision.com.cn>
2025-12-30 22:59:03 +0800

    sys config

--- CVDB/Services/Buz/WaferProberDBService.cs
+++ CVDB/Services/Buz/WaferProberDBService.cs
@@ -37,5 +37,54 @@ namespace CVDB.Services.Buz
 
             return null;
         }
+
+        public static void InitBuzWaferProber_10001()
+        {
+            var master = MysqlControler.GetInstance().Sql.Select<TScgdBuzProductMaster>().Where(a => a.BuzType == 10001 && a.IsEnable == 1 && a.IsDelete == 0).ToOne();
+            if (master == null)
+            {
+                master = new TScgdBuzProductMaster() { BuzType = 10001, Code = "WaferProber.Flow", Name = "晶圆探针台流程", IsEnable = 1, IsDelete = 0 };
+                long id = MysqlControler.GetInstance().Sql.Insert(master).ExecuteIdentity();
+                if (id > 0)
+                {
+                    master.Id = (int)id;
+                }
+            }
+            if (master.Id > 0)
+            {
+                var buzDetails = MysqlControler.GetInstance().Sql.Select<TScgdBuzProductDetail>().Where(a => a.Pid == master.Id).ToList();
+                if (buzDetails == null || buzDetails.Count == 0)
+                {
+                    TScgdBuzProductDetail buzProductDetail = new TScgdBuzProductDetail() { Pid = master.Id, Code= "Flow.AOI" };
+                    MysqlControler.GetInstance().Sql.Insert(buzProductDetail).ExecuteAffrows();
+                    buzProductDetail = new TScgdBuzProductDetail() { Pid = master.Id, Code = "Flow.IVL.SP" };
+                    MysqlControler.GetInstance().Sql.Insert(buzProductDetail).ExecuteAffrows();
+                    buzProductDetail = new TScgdBuzProductDetail() { Pid = master.Id, Code = "Flow.IVL.Camera" };
+                    MysqlControler.GetInstance().Sql.Insert(buzProductDetail).ExecuteAffrows();
+                    buzProductDetail = new TScgdBuzProductDetail() { Pid = master.Id, Code = "Flow.EQE" };
+                    MysqlControler.GetInstance().Sql.Insert(buzProductDetail).ExecuteAffrows();
+                    buzProductDetail = new TScgdBuzProductDetail() { Pid = master.Id, Code = "Flow.VAM" };
+                    MysqlControler.GetInstance().Sql.Insert(buzProductDetail).ExecuteAffrows();
+                }
+                else
+                {
+                    foreach (var buzProductDetail in buzDetails)
+                    {
+                        buzProductDetail.Name = null;
+                        MysqlControler.GetInstance().Sql.Update<TScgdBuzProductDetail>().SetSource(buzProductDetail).ExecuteAffrows();
+                    }
+                }
+            }
+        }
+
+        public static TScgdBuzProductDetail GetBuzDetail(int id)
+        {
+            return MysqlControler.GetInstance().Sql.Select<TScgdBuzProductDetail>().Where(a => a.Id == id).ToOne();
+        }
+
+        public static void UpdateBuzDetail(TScgdBuzProductDetail buzProductDetail)
+        {
+            MysqlControler.GetInstance().Sql.Update<TScgdBuzProductDetail>().SetSource(buzProductDetail).ExecuteAffrows();
+        }
     }
 }
--- CVWaferProber/Views/SysFlowCfgWindow.xaml.cs
+++ CVWaferProber/Views/SysFlowCfgWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿using CVDB.Services.Buz;
-using System;
-using System.Collections.Generic;
+﻿using ColorVision.Core.Entities;
+using CVDB.Services.Buz;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CVWaferProber.Views
 {
@@ -71,35 +62,41 @@ namespace CVWaferProber.Views
         }
         private void LoadConfigData()
         {
-            ConfigItems = new ObservableCollection<FlowConfigItem>();
-
+            if (ConfigItems == null) ConfigItems = new ObservableCollection<FlowConfigItem>();
+            ConfigItems.Clear();
+            List<TScgdBuzProductDetail> buz_flows = WaferProberDBService.LoadBuzFlows();
+            if (buz_flows == null || buz_flows.Count == 0)
+            {
+                return;
+            }
             var flows = WaferProberDBService.LoadAllFlows();
             // 模拟数据 - 假设有不同类型的配置项
-            var optionTrueFalse = new ObservableCollection<ConfigOption>();
+            var flowAll = new ObservableCollection<ConfigOption>();
+            var option_emp = new ConfigOption() { DisplayName = "空", Value = null };
+            flowAll.Add(option_emp);
             foreach (var flow in flows)
             {
                 var option = new ConfigOption() {  DisplayName = flow.Name , Value = flow.Name };
-                optionTrueFalse.Add(option);
+                flowAll.Add(option);
             }
-            // 添加配置项
-            ConfigItems.Add(new FlowConfigItem
-            {
-                DisplayName = "AOI",
-                ValueType = "AOI",
-                AvailableOptions = optionTrueFalse,
-                SelectedValue = optionTrueFalse[0] // 默认选择第一个
-            });
 
-            ConfigItems.Add(new FlowConfigItem
+            foreach (var flow in buz_flows)
             {
-                DisplayName = "IVL",
-                ValueType = "IVL",
-                AvailableOptions = optionTrueFalse,
-                SelectedValue = optionTrueFalse[0] // 默认选择"中"
-            });
+                var option = string.IsNullOrEmpty(flow.Name)
+                    ? flowAll[0]
+                    : flowAll.FirstOrDefault(item => item.DisplayName == flow.Name) ?? flowAll[0];
 
-            // ... 可以添加更多配置项
+                ConfigItems.Add(new FlowConfigItem
+                {
+                    Id = flow.Id,
+                    DisplayName = flow.Code,
+                    ValueType = flow.Code,
+                    AvailableOptions = flowAll,
+                    SelectedValue = option
+                });
+            }
         }
+
         // 处理行加载事件，设置行号（序号）
         private void ConfigDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
         {
@@ -108,6 +105,13 @@ namespace CVWaferProber.Views
         }
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+            foreach (var item in ConfigItems)
+            {
+                TScgdBuzProductDetail buzProductDetail = WaferProberDBService.GetBuzDetail(item.Id);
+                buzProductDetail.Name = item.SelectedValue.Value;
+                WaferProberDBService.UpdateBuzDetail(buzProductDetail);
+            }
+            MessageBox.Show("Save Completed");
         }
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
@@ -117,11 +121,13 @@ namespace CVWaferProber.Views
             if (result == MessageBoxResult.Yes)
             {
                 InitializeData();
+                LoadConfigData();
             }
         }
 
         private void InitializeData()
         {
+            WaferProberDBService.InitBuzWaferProber_10001();
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -133,6 +139,7 @@ namespace CVWaferProber.Views
     // 配置项数据模型
     public class FlowConfigItem : INotifyPropertyChanged
     {
+        public int Id { get; set; }
         public string DisplayName { get; set; }
         public string ValueType { get; set; }
         public ObservableCollection<ConfigOption> AvailableOptions { get; set; }
