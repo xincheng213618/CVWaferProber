7676809213af3fa02778d37e5de8d988985fe04b
21158 <21158@GU>
2026-01-28 12:17:24 +0800

    1

--- CVAVMControl/CVVAMAnalyzer.xaml.cs
+++ CVAVMControl/CVVAMAnalyzer.xaml.cs
@@ -53,7 +53,7 @@ namespace CVAVMControl
         private System.Windows.Point center;
         private int imageRadius;
         private double MaxAngle = 60; // Default max angle
-        private double ConoscopeCoefficient = 0.02645; // Pixels per degree   0.01935 0.02645
+        private double ConoscopeCoefficient = 0.01935; // Pixels per degree   0.01935 0.02645
 
         private int displayAngle = 120; // Default display angle
         private ExportChannel displayChannel = ExportChannel.Y; // Default display channel
