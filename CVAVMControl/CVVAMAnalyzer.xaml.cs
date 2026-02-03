abfea332dbdcba11a3b2bfc92c98ae10e9ab4b5d
chenhong <chenhong@color-vision.com.cn>
2026-02-03 09:14:22 +0800

    VAM

--- CVAVMControl/CVVAMAnalyzer.xaml.cs
+++ CVAVMControl/CVVAMAnalyzer.xaml.cs
@@ -54,7 +54,7 @@ namespace CVAVMControl
         private System.Windows.Point center;
         private int imageRadius;
         private double MaxAngle = 60; // Default max angle
-        private double ConoscopeCoefficient = 0.01935; // Pixels per degree   0.01935 0.02645
+        private double ConoscopeCoefficient = 0.028735632183908;// 0.01935; // Pixels per degree   0.01935 0.02645
 
         private int displayAngle = 120; // Default display angle
         private ExportChannel displayChannel = ExportChannel.Y; // Default display channel
